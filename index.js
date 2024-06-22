aaf51fbf9280fd0b370a810c5425f02c41b3bf23
uzairzia <uzair.governorsindh@gmail.com>
2024-06-22 18:26:00 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+function countWords(paragraph) {
+    let freeWhiteSpaces = paragraph.replace(/\s/g, "");
+    return freeWhiteSpaces.length;
+}
+async function startApp() {
+    do {
+        const answers = await inquirer.prompt([
+            {
+                name: "paragraph",
+                type: "input",
+                message: "Enter the paragraph to count words:",
+            },
+        ]);
+        let wordCount = countWords(answers.paragraph);
+        console.log(`The paragraph contains ${wordCount} words.`);
+    } while (true);
+}
+startApp();
---
+++ index.ts
@@ -0,0 +1,27 @@
+/*
+create a word Counter app to count words by taking input from the user
+*/
+import { log } from "console";
+import inquirer from "inquirer";
+
+function countWords(paragraph: string) {
+  let freeWhiteSpaces = paragraph.replace(/\s/g, "");
+
+  return freeWhiteSpaces.length;
+}
+
+async function startApp() {
+  do {
+    const answers = await inquirer.prompt([
+      {
+        name: "paragraph",
+        type: "input",
+        message: "Enter the paragraph to count words:",
+      },
+    ]);
+
+    let wordCount = countWords(answers.paragraph);
+    console.log(`The paragraph contains ${wordCount} words.`);
+  } while (true);
+}
+startApp();
